72426a09bf456bc194468e8cf3b4f0a8809ef8c0
João Victor Ventura Martins <jvmartinscv@gmail.com>
2026-08-02 15:40:13 -0300

    feat(server): implementa entrypoint com migrations e shutdown gracioso

---
+++ cmd/server/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/JoaoVictorVM/ludora/internal/config"
+	"github.com/JoaoVictorVM/ludora/internal/database"
+	"github.com/JoaoVictorVM/ludora/internal/logging"
+	"github.com/JoaoVictorVM/ludora/internal/router"
+)
+
+const (
+	staticDir         = "static"
+	readHeaderTimeout = 10 * time.Second
+	shutdownTimeout   = 15 * time.Second
+)
+
+func main() {
+	if err := run(); err != nil {
+		// The logger may not exist yet when configuration fails, so this last
+		// resort goes straight to stderr before exiting.
+		os.Stderr.WriteString("fatal: " + err.Error() + "\n")
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	cfg, err := config.Load()
+	if err != nil {
+		return err
+	}
+
+	logger := logging.New(cfg.Env)
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	pool, err := database.NewPool(ctx, cfg.DatabaseURL)
+	if err != nil {
+		return err
+	}
+	defer pool.Close()
+
+	if err := database.Migrate(ctx, pool, logger); err != nil {
+		return err
+	}
+
+	server := &http.Server{
+		Addr:              ":" + cfg.Port,
+		Handler:           router.New(logger, staticDir),
+		ReadHeaderTimeout: readHeaderTimeout,
+	}
+
+	serverErr := make(chan error, 1)
+	go func() {
+		logger.Info("server listening", "port", cfg.Port, "env", cfg.Env)
+		if err := server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+			serverErr <- err
+		}
+	}()
+
+	select {
+	case err := <-serverErr:
+		return err
+	case <-ctx.Done():
+		logger.Info("shutting down")
+	}
+
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), shutdownTimeout)
+	defer cancel()
+
+	return server.Shutdown(shutdownCtx)
+}
